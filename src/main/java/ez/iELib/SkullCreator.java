0e727cfc528d8c8b6cc870c7e912baee8a751771
TheRealEzCoins <stevens.ian633@gmail.com>
2024-11-28 19:30:08 +0100

    Added SkullCreator to the library

---
+++ src/main/java/ez/iELib/SkullCreator.java
@@ -0,0 +1,218 @@
+package ez.iELib;
+
+import com.destroystokyo.paper.profile.PlayerProfile;
+import com.destroystokyo.paper.profile.ProfileProperty;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.SkullType;
+import org.bukkit.block.Block;
+import org.bukkit.block.Skull;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Base64;
+import java.util.UUID;
+
+public class SkullCreator {
+
+    private static boolean warningPosted = false;
+
+
+    /**
+     * Creates a player skull, should work in both legacy and new Bukkit APIs.
+     */
+    public static ItemStack createSkull() {
+        checkLegacy();
+
+        try {
+            return new ItemStack(Material.valueOf("PLAYER_HEAD"));
+        } catch (IllegalArgumentException e) {
+            return new ItemStack(Material.valueOf("SKULL_ITEM"), 1, (byte) 3);
+        }
+    }
+
+    /**
+     * Creates a player skull item with the skin based on a player's name.
+     *
+     * @param name The Player's name.
+     * @return The head of the Player.
+     * @deprecated names don't make for good identifiers.
+     */
+    public static ItemStack itemFromName(String name) {
+        return itemWithName(createSkull(), name);
+    }
+
+    /**
+     * Creates a player skull item with the skin based on a player's UUID.
+     *
+     * @param id The Player's UUID.
+     * @return The head of the Player.
+     */
+    public static ItemStack itemFromUuid(UUID id) {
+        return itemWithUuid(createSkull(), id);
+    }
+
+    /**
+     * Creates a player skull item with the skin at a Mojang URL.
+     *
+     * @param url The Mojang URL.
+     * @return The head of the Player.
+     */
+    public static ItemStack itemFromUrl(String url) {
+        return itemWithUrl(createSkull(), url);
+    }
+
+    /**
+     * Creates a player skull item with the skin based on a base64 string.
+     *
+     * @param base64 The Base64 string.
+     * @return The head of the Player.
+     */
+    public static ItemStack itemFromBase64(String base64) {
+        return itemWithBase64(createSkull(), base64);
+    }
+
+    /**
+     * Modifies a skull to use the skin of the player with a given name.
+     *
+     * @param item The item to apply the name to. Must be a player skull.
+     * @param name The Player's name.
+     * @return The head of the Player.
+     * @deprecated names don't make for good identifiers.
+     */
+    @Deprecated
+    public static ItemStack itemWithName(ItemStack item, String name) {
+        notNull(item, "item");
+        notNull(name, "name");
+
+        SkullMeta meta = (SkullMeta) item.getItemMeta();
+        meta.setOwner(name);
+        item.setItemMeta(meta);
+
+        return item;
+    }
+
+    /**
+     * Modifies a skull to use the skin of the player with a given UUID.
+     *
+     * @param item The item to apply the name to. Must be a player skull.
+     * @param id   The Player's UUID.
+     * @return The head of the Player.
+     */
+    public static ItemStack itemWithUuid(ItemStack item, UUID id) {
+        notNull(item, "item");
+        notNull(id, "id");
+
+        SkullMeta meta = (SkullMeta) item.getItemMeta();
+        meta.setOwningPlayer(Bukkit.getOfflinePlayer(id));
+        item.setItemMeta(meta);
+
+        return item;
+    }
+
+    /**
+     * Modifies a skull to use the skin at the given Mojang URL.
+     *
+     * @param item The item to apply the skin to. Must be a player skull.
+     * @param url  The URL of the Mojang skin.
+     * @return The head associated with the URL.
+     */
+    public static ItemStack itemWithUrl(ItemStack item, String url) {
+        notNull(item, "item");
+        notNull(url, "url");
+
+        return itemWithBase64(item, urlToBase64(url));
+    }
+
+    /**
+     * Modifies a skull to use the skin based on the given base64 string.
+     *
+     * @param item   The ItemStack to put the base64 onto. Must be a player skull.
+     * @param base64 The base64 string containing the texture.
+     * @return The head with a custom texture.
+     */
+    public static ItemStack itemWithBase64(ItemStack item, String base64) {
+        notNull(item, "item");
+        notNull(base64, "base64");
+
+        if (!(item.getItemMeta() instanceof SkullMeta)) {
+            return null;
+        }
+        SkullMeta meta = (SkullMeta) item.getItemMeta();
+
+        String texture = base64;
+
+        PlayerProfile profile = makeProfile(texture);
+
+        profile.setProperty(new ProfileProperty("textures", texture));
+        meta.setPlayerProfile(profile);
+
+        item.setItemMeta(meta);
+
+        return item;
+    }
+
+    private static void setToSkull(Block block) {
+        checkLegacy();
+
+        try {
+            block.setType(Material.valueOf("PLAYER_HEAD"), false);
+        } catch (IllegalArgumentException e) {
+            block.setType(Material.valueOf("SKULL"), false);
+            Skull state = (Skull) block.getState();
+            state.setSkullType(SkullType.PLAYER);
+            state.update(false, false);
+        }
+    }
+
+    private static void notNull(Object o, String name) {
+        if (o == null) {
+            throw new NullPointerException(name + " should not be null!");
+        }
+    }
+
+    private static String urlToBase64(String url) {
+
+        URI actualUrl;
+        try {
+            actualUrl = new URI(url);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        String toEncode = "{\"textures\":{\"SKIN\":{\"url\":\"" + actualUrl.toString() + "\"}}}";
+        return Base64.getEncoder().encodeToString(toEncode.getBytes());
+    }
+
+    private static PlayerProfile makeProfile(String b64) {
+        // random uuid based on the b64 string
+        UUID id = new UUID(
+                b64.substring(b64.length() - 20).hashCode(),
+                b64.substring(b64.length() - 10).hashCode()
+        );
+
+        return Bukkit.createProfile(id);
+    }
+
+
+    // suppress warning since PLAYER_HEAD doesn't exist in 1.12.2,
+    // but we expect this and catch the error at runtime.
+    @SuppressWarnings("JavaReflectionMemberAccess")
+    private static void checkLegacy() {
+        try {
+            // if both of these succeed, then we are running
+            // in a legacy api, but on a modern (1.13+) server.
+            Material.class.getDeclaredField("PLAYER_HEAD");
+            Material.valueOf("SKULL");
+
+            if (!warningPosted) {
+                Bukkit.getLogger().warning("SKULLCREATOR API - Using the legacy bukkit API with 1.13+ bukkit versions is not supported!");
+                warningPosted = true;
+            }
+        } catch (NoSuchFieldException | IllegalArgumentException ignored) {}
+    }
+
+
+
+}
