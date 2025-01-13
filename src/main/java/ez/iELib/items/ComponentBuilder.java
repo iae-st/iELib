4b2ab01f1ce3f58dd6121deab7991185a23096f3
TheRealEzCoins <stevens.ian633@gmail.com>
2025-01-13 17:26:36 +0100

    Components suck

--- src/main/java/ez/iELib/items/ComponentBuilder.java
+++ src/main/java/ez/iELib/items/ComponentBuilder.java
@@ -1,23 +1,17 @@
 package ez.iELib.items;
 
+import ez.iELib.utils.colorUtils.ColorUtils;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 
 public class ComponentBuilder {
     public static Component createComponent(String text) {
-        String miniMessageString = MiniMessage.miniMessage().serialize(
-                LegacyComponentSerializer.legacyAmpersand().deserialize(text));
+        Component sectionComponent = LegacyComponentSerializer.legacySection().deserialize(text);
+        String legacyAmpersand = LegacyComponentSerializer.legacyAmpersand().serialize(sectionComponent);
+        Component ampersandComponent = LegacyComponentSerializer.legacyAmpersand().deserialize(legacyAmpersand);
+        String miniMessageString = MiniMessage.miniMessage().serialize(ampersandComponent);
         return MiniMessage.miniMessage().deserialize("<!i>" + miniMessageString);
     }
 
-
-    public static Component createComponent(String text, String... replacements) {
-        String miniMessageString = MiniMessage.miniMessage().serialize(
-                LegacyComponentSerializer.legacyAmpersand().deserialize(text));
-        for (String replacement : replacements) {
-            miniMessageString = miniMessageString.replaceFirst("%s", replacement);
-        }
-        return MiniMessage.miniMessage().deserialize("<!i>" + miniMessageString);
-    }
 }
