e454df0e1d3890a5981325f5f39503d07c19c2e4
TheRealEzCoins <stevens.ian633@gmail.com>
2025-01-15 15:56:07 +0100

    Updated ComponentBuilder.java

--- src/main/java/ez/iELib/items/ComponentBuilder.java
+++ src/main/java/ez/iELib/items/ComponentBuilder.java
@@ -3,15 +3,20 @@ package ez.iELib.items;
 import ez.iELib.utils.colorUtils.ColorUtils;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
+import net.kyori.adventure.text.minimessage.tag.standard.StandardTags;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 
 public class ComponentBuilder {
     public static Component createComponent(String text) {
-        Component sectionComponent = LegacyComponentSerializer.legacySection().deserialize(text);
-        String legacyAmpersand = LegacyComponentSerializer.legacyAmpersand().serialize(sectionComponent);
-        Component ampersandComponent = LegacyComponentSerializer.legacyAmpersand().deserialize(legacyAmpersand);
-        String miniMessageString = MiniMessage.miniMessage().serialize(ampersandComponent);
-        return MiniMessage.miniMessage().deserialize("<!i>" + miniMessageString);
+        MiniMessage mm = MiniMessage.builder()
+                .tags(
+                        TagResolver.builder()
+                                .resolver(StandardTags.defaults())
+                                .build()
+                )
+                .build();
+        return mm.deserialize(text);
     }
 
 }
